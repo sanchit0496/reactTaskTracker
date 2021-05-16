90ae1dcb22eb3125276f66df995569edacf96ba5
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2021-05-16 18:02:42 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const About = () => {
+    return (
+        <div>
+            <h4>Version 1.0</h4>
+            <Link to = "/">Navigate Back</Link>
+        </div>
+    )
+}
+
+export default About
